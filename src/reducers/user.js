330a137070ce0f76afdcf2d0fc17f57ca7731cb6
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-05-18 18:24:39 -0300

    Adiciona os aquivos index e user na pasta reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,18 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { INSERT_EMAIL } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, actions) => {
+  switch (actions.type) {
+  case INSERT_EMAIL:
+    return {
+      email: actions.email,
+    };
+  default:
+    return state;
+  }
+};
+
+export default user;
