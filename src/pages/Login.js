8e1c37fa1d5771f4976cb4be9bb1e8bf14f7b9ac
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-05-18 18:18:41 -0300

    Adiciona a cirção da paginá de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,77 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Input from '../components/Input';
+import { actionLogin } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+      redirect: false,
+    };
+  }
+
+  emailVerify = () => {
+    const { email, password } = this.state;
+    const min = 6;
+    const ruleToEnable = (/[\w]+@+[\w]+.com/.test(email) && password.length < min);
+    this.setState({ disabled: ruleToEnable });
+  };
+
+  onChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, this.emailVerify());
+  };
+
+  onClick = (e) => {
+    const { email } = this.state;
+    const { login } = this.props;
+    e.preventDefault();
+    login(email);
+    this.setState({ redirect: true });
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password, disabled, redirect } = this.state;
+    const { onChange, onClick } = this;
+    return (
+      <section>
+        <Input
+          type="email"
+          name="email"
+          value={ email }
+          placeholder="Email"
+          onChange={ onChange }
+          data-testid="email-input"
+        />
+        <Input
+          type="password"
+          name="password"
+          value={ password }
+          placeholder="Senha"
+          onChange={ onChange }
+          data-testid="password-input"
+        />
+        <button type="submit" onClick={ onClick } disabled={ disabled }>
+          Entrar
+        </button>
+        { redirect && <Redirect to="/carteira" /> }
+      </section>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  login: (email) => dispatch(actionLogin(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
