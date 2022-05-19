fc93bdd789e48f6c5a2ab1a5078edf4e5756ee1f
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-05-19 02:57:29 -0300

    Adiciona criação do componente Coins

---
+++ src/components/Coins.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Coins extends React.Component {
+  render() {
+    const { value, onChange, currencies } = this.props;
+
+    return (
+      <label htmlFor="coin">
+        Moeda:
+        <select value={ value } id="coin" onChange={ onChange }>
+          {currencies.map((currency, index) => (
+            <option key={ index }>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </label>
+    );
+  }
+}
+
+Coins.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  currencies: PropTypes.array,
+}.isRequired;
+
+export default Coins;
