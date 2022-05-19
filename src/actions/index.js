d24240c53806a04e8876abbaeae572e3ed754b7f
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-05-19 18:50:25 -0300

    Adiciona o requisito 7 :rocket:

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const currencies = (currency) => ({ type: 'CURRENCIES', currency });
 
 export const addPay = (result, paymentInfo) => ({ type: 'ADD_PAY', paymentInfo, result });
 
-export const delePay = (id) => ({ type: 'DELETE_PAY', id });
+export const deletPay = (id) => ({ type: 'DELETE_PAY', id });
 
 export const editPay = (paymentState) => ({ type: 'EDIT_PAY', paymentState });
 
