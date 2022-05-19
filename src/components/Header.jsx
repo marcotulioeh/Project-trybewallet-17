9b983da0cb95903fa6c27f781c782815aa005f10
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-05-18 23:57:03 -0300

    Adiciona requisito 3 :rocket:

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { currencies, awsomeFetch } from '../actions';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount = () => {
+    const { currenciesFetch } = this.props;
+    currenciesFetch();
+  };
+
+  render() {
+    const { email, resulTotal } = this.props;
+
+    return (
+      <header>
+        <div data-testid="email-field">
+          Email:
+          { email }
+        </div>
+        <div data-testid="total-field">
+          Despesas total: R$
+          { parseFloat(resulTotal).toFixed(2) }
+        </div>
+        <div data-testid="header-currency-field">
+          BRL
+        </div>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string,
+  currenciesFetch: PropTypes.string,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  resulTotal: state.wallet.expenses
+    .reduce((
+      acc,
+      { value, currency, exchangeRates },
+    ) => acc + (parseFloat(exchangeRates[currency].ask) * value), 0) });
+
+const mapDispatchToProps = (dispatch) => ({
+  currenciesFetch: () => dispatch(awsomeFetch(currencies)) });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,45 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'CURRENCIES':
+    return {
+      ...state,
+      // currencies: Object.keys(action.currencies),
+    };
+  case 'ADD_PAY':
+    return {
+      ...state,
+      expenses: [
+        ...state.expenses,
+        {
+          ...action.paymentInfo,
+          id: state.expenses.length,
+          exchangeRates: action.result,
+        },
+      ],
+    };
+  case 'DELETE_PAY':
+    return {
+      ...state,
+      expenses: state.expenses
+        .filter((payment) => payment.id !== action.id),
+    };
+  case 'EDIT_PAY':
+    return {
+      ...state,
+      expenses: [
+        ...state.expenses
+          .filter((payment) => payment.id !== action.paymentState.id),
+        action.paymentState,
+      ].sort((a, b) => a.id - b.id),
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
